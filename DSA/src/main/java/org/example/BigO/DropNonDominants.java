121d4b47ba05b07b8887325b6cb39203365409a0
jabezasir421-afk <jabezasir421@gmail.com>
2026-09-19 20:42:05 +0530

    Drop Non dominant

---
+++ DSA/src/main/java/org/example/BigO/DropNonDominants.java
@@ -0,0 +1,23 @@
+package org.example.BigO;
+
+public class DropNonDominants {
+    /*
+ Drop non-dominant example
+ here bigO(n power 2)
+   */
+    static void main() {
+        printNumbers(10);
+    }
+
+    public static void printNumbers(int n){
+        for (int i =0;i<n;i++){
+            for (int j =0;j<n;j++){
+                System.out.println(i+" "+j);
+            }
+            for (int k=0;k<n;k++){
+                IO.println(k);
+            }
+        }
+
+    }
+}
